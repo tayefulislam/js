14c6b731e604d845427f007d4458269b051098c2
tayefulislam <kazitayefulislam12@gmail.com>
2024-10-14 21:09:30 +0600

    data types review

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+// number
+// string
+// bigint = big number
+// boolean = true or false
+// undefined
+// null  ( object ) = stanelone value
+// symbol = unique value
