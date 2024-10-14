d58353967d867633d6c3faf464b07416cd2f25bd
tayefulislam <kazitayefulislam12@gmail.com>
2024-10-14 20:01:01 +0600

    basic done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,17 @@
-console.log("Tayeful")
+const accountId = 98793;
+let date = "Jan 12,2022";
+let userName = "Kazi Tayeful Islam";
+
+var email = "tayef@t.c";
+welcome = "Welcome";
+
+/*
+Prefer not to use , var 
+
+or 
+
+Don't diclur variables without let or const
+
+*/
+
+console.log([accountId, userName, date, email, welcome]);
