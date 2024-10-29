bc337b67d9efaa99cf2fa62d7fd8981ed6441707
tayefulislam <kazitayefulislam12@gmail.com>
2024-10-29 19:08:11 +0600

    string pratice

---
+++ 01_basics/05_string.js
@@ -0,0 +1,30 @@
+const name = new String("Kita Mori");
+const nameN = "Kita Mori";
+
+// uppercase
+
+// console.log(name.toUpperCase());
+
+// chat At
+
+// console.log(name.charAt(0));
+// console.log(nameN.indexOf("a"));
+
+// bold
+
+// console.log(name.bold());
+/*
+.bold can use in to add <br> tag 
+
+*/
+
+/* .endWith() can use in password match system 
+*/
+// console.log(nameN.endsWith(name));
+
+
+
+
+
+
+
