6d4385c0eaf106d7a0976327f3f0b95b8c630953
tayefulislam <kazitayefulislam12@gmail.com>
2024-11-07 21:11:35 +0600

    nums maths comit

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -10,7 +10,49 @@
 // console.log(balance.toPrecision(5));
 
 
-let num = 1000000;
+// let num = -9007199254740991 ;
+// console.log(num.toString());
+// console.log(Math.abs(num).toString());
 
-console.log(num.toLocaleString("en-IN"));
+// let num = 2;
+
+// let numArr = (94, 432, 34, 56, 654, 45, 345);
+// console.log(Math.sqrt(num));
+//
+// console.log(Math.random(10000));
+
+
+// Original Math.random() give value between 0 to 1.
+// - It can return 0 value also , so always use add 1 ( randomValue + 1)
+
+
+console.log(Math.random() * 10);
+
+const min = 1000;
+const max = 2000;
+
+for (let i = 0; i < 1000; i++) {
+
+    const makediOne = Math.floor(Math.random() * 10);
+    console.log(makediOne)
+
+    const ThreeDi = Math.floor(Math.random() * (max - min + 1));
+
+
+
+    console.log(makediOne+ "" +ThreeDi);
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
 
