332285dab47681c4299f3eb9735a93befa5a3f2d
Tayeful Islam <96137687+tayefulislam@users.noreply.github.com>
2024-11-07 15:09:57 +0000

    date push from codespace

--- 01_basics/07_dateAndTimeInJS.js
+++ 01_basics/07_dateAndTimeInJS.js
@@ -1,7 +1,12 @@
-const today = new Date();
-console.log(today);
+// const today = new Date();
+// console.log(today);
+// console.log(today.toDateString());
+// console.log(today.toISOString());
+// console.log(today.toLocaleTimeString());
+
+const today = new Date("1730982001580");
 console.log(today.toDateString());
-console.log(today.toISOString());
-console.log(today.toLocaleTimeString());
+
+
 
 
