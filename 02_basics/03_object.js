d85c8c844dee84d658fcb71c0d69a2dd3677d86f
tayefulislam <kazitayefulislam12@gmail.com>
2024-11-13 20:25:04 +0600

    learn about object.freeze() method , and learn about this keyboard same object side property access system

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -27,11 +27,43 @@ const userOne = {
 
 // console.log(userOne["old address"]); // another system [ string value with space in key  ]
 
-console.log(mySym1 === mySym2);
+// console.log(mySym1 === mySym2);
 
 // Symbol example in real life
 // Suppose we have a private key and we dont want to expose it to the outside world,
 // Then we can use symbol in the object property name.
 // In real life, Symbols are used for creating unique property keys to avoid collisions when using data from external sources.
-// more please 
+// more please
  
+
+// Object.freeze() Section
+
+// change the value
+userOne.email = "tushar@gmail.oo";
+
+// console.log(userOne.email);
+
+// Object.freeze(userOne);
+
+// userOne.email = "trytocshange@gmail.com";
+
+// console.log(userOne.email);
+
+userOne.objectFun = function () {
+    console.log("Hello function from inside object");
+}
+
+userOne.objectFunTwo = function () {
+    console.log(`Hello Public, This is  ${this.email} `);
+    //here we use this keyword to access object property (this.email)
+    // if we try to access direct (email) it will give {ReferenceError: email is not defined } 
+}
+
+// console.log(userOne);
+
+
+console.log(userOne.objectFun());
+console.log(userOne.objectFunTwo());
+
+
+
